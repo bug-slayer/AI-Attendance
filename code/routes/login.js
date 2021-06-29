ba3cc50e652a0d6ecc0b3430b5110bc0eaac6805
juff <15620902461>
2021-06-29 10:26:36 +0800

    添加了前端layui模板和数据库sqliteadmin

--- code/routes/login.js
+++ code/routes/login.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var SqliteDB = require('sqlite3').SqliteDB;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('loginplease');
+  var username=req.query['username'];
+  var password=req.query['password'];
+  var sql= "select cout(1) from user where username="+username+" and password= "+password;
+  var db=new SqliteDB('../db/aiattendance.s3db');
+  db.get(sql,function(data){
+    console.log(data);
+
+  })
+db.close();
+
+
+
+  res.send('0');
 });
 
 module.exports = router;
